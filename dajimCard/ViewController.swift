7105e36f88e6befa6e3d78c17203e866229ea256
hemssy <hemsyeons@gmail.com>
2025-08-18 12:07:52 +0900

    Initial Commit

---
+++ dajimCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dajimCard
+//
+//  Created by 신서연 on 8/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
